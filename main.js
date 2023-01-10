9e6a17786b02a93c1ab680e4bd1d1c186b4cab81
sufian015 <ab.sufian1997@gmail.com>
2023-01-10 05:11:25 -0800

    first-commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // const express=require('express');
 // app=express();
 
-// // query
+// // query//
 //  app.get('/',(req,res)=>{
 //      let firstname=req.query.firstname;
 //      let lastname=req.query.lastname;
@@ -10,7 +10,15 @@
 
 //  })
 
-//  // header
+//  app.post('/',(req,res)=>{
+//      let firstname=req.query.firstname;
+//      let lastname=req.query.lastname;
+//      res.end(`${firstname} ${lastname}`);
+
+
+//  })
+
+//  // header//
 //  app.get('/',(req,res)=>{
 //    let firstName=req.header('firstName');
 //    let UserAgent=req.header('User-Agent');
@@ -27,11 +35,73 @@
 
 //  })
 
-//  // this is download response operation
+// response header//
+
+// app.get('/five',(req,res)=>{
+//      res.append('name','rabbil hasan')
+//      res.append('city','Dhaka')
+//      res.append('age','30')
+//      res.status(201).end('this is five');
+//  });
+
+//  // this is download response operation//
 
 // app.get('/four',(req,res)=>{
 //      res.download('./picture/abc.png')
 // });
+
+//file upload//
+
+const express=require('express');
+const multer=require('multer');
+
+app=express();
+ let storage=multer.diskStorage({destination:(req,file,callback)=>{
+     callback(null,'./picture')
+ }},
+ {
+     filename:(req,file,callback)=>{
+          callback(null,file.originalname)
+
+     }
+ }
+     
+)
+let upload=multer({storage:storage}).single('myfile');
+
+app.post('/',function(req,res){
+     upload(req,res,function(err){
+          if(err){
+               res.send('file upload fail')
+          }else{res.send('file upload successful')}
+     })
+
+});
+app.listen(3000,function(){
+     console.log('server running');
+})
+
+// body //
+// const express=require('express');
+// const bodyParser=require('body-parser');
+// app=express();
+// app.use(bodyParser.json());
+
+// app.post('/',(req,res)=>{
+//      let jsonData=req.body;
+//     let name=jsonData['name']
+//     let city=jsonData['city']
+//     res.send(`${name} ${city}`);
+// })
+
+
+
+
+
+
+// app.listen(3000,()=>{
+//      console.log('server run success');
+// })
 
 
 
